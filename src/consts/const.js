641725834c156f5a757234e90f5ee59e22b0b401
pmes18 <pmartinsesa@gmail.com>
2021-06-12 14:09:50 -0300

    versão final

--- src/consts/const.js
+++ src/consts/const.js
@@ -1,4 +1,4 @@
-export const usersUrl = 'http://localhost:4000/users';
-export const activesUrl = 'http://localhost:4000/actives';
-export const companiesUrl = 'http://localhost:4000/companies';
-export const unitsUrl = 'http://localhost:4000/units';
+export const usersUrl = `${process.env.REACT_APP_API_URL}/users`;
+export const activesUrl = `${process.env.REACT_APP_API_URL}/actives`;
+export const companiesUrl = `${process.env.REACT_APP_API_URL}/companies`;
+export const unitsUrl = `${process.env.REACT_APP_API_URL}/units`;
